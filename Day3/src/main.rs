704e75897b1a65238d4fb8e68c090b73688bb8e9
KARMELbtw <machnikkamil9@gmail.com>
2025-09-05 16:59:40 +0200

    Day3 part2

--- Day3/src/main.rs
+++ Day3/src/main.rs
@@ -5,34 +5,62 @@ fn main() -> std::io::Result<()>{
     let input = fs::read_to_string("input.txt")?;
 
     let mut houses_visited = 1;
-    let mut x = 0;
-    let mut y = 0;
+    // let mut x = 0;
+    // let mut y = 0;
+    let mut x1 = 0;
+    let mut y1 = 0;
+    let mut x2 = 0;
+    let mut y2 = 0;
+    let mut turn = 0;
 
     let mut houses_visited_cords : HashSet<(i32, i32)> = HashSet::new();
+    houses_visited_cords.insert((0,0));
 
     for c in input.chars() {
-        match c{
-            '<'=> {
-                x -= 1;
+        // match c{
+        //     '<'=> x -= 1;
+        //     '^'=> y += 1;
+        //     '>'=> x += 1;
+        //     'v'=> y -= 1;
+        //     _ => {}
+        // }
+        // if !houses_visited_cords.contains(&(x, y)) {
+        //     houses_visited += 1;
+        //     houses_visited_cords.insert((x,y));
+        // }
+
+        if turn%2 == 0 {
+            match c
+            {
+                '<'=> x1 -= 1,
+                '^'=> y1 += 1,
+                '>'=> x1 += 1,
+                'v'=> y1 -= 1,
+                _ => {}
             }
-            '^'=> {
-                y += 1;
+            if !houses_visited_cords.contains(&(x1, y1)) {
+                houses_visited += 1;
+                houses_visited_cords.insert((x1,y1));
             }
-            '>'=> {
-                x += 1;
+        } else {
+            match c
+            {
+                '<'=> x2 -= 1,
+                '^'=> y2 += 1,
+                '>'=> x2 += 1,
+                'v'=> y2 -= 1,
+                _ => {}
             }
-            'v'=> {
-                y -= 1;
+            if !houses_visited_cords.contains(&(x2, y2)) {
+                houses_visited += 1;
+                houses_visited_cords.insert((x2,y2));
             }
-            _ => {}
         }
-        if !houses_visited_cords.contains(&(x, y)) {
-            houses_visited += 1;
-            houses_visited_cords.insert((x,y));
-        }
+        turn += 1;
     }
 
-    println!("First Answer: {}" ,houses_visited);
+    // println!("First Answer: {}" ,houses_visited);
+    println!("Second Answer: {}" ,houses_visited);
 
     Ok(())
 }
