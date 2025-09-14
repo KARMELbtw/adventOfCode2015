514bcdb246be1de6c855a14d5c484c892d33c160
KARMELbtw <machnikkamil9@gmail.com>
2025-09-14 19:54:36 +0200

    i don't even know

--- Day9/src/main.rs
+++ Day9/src/main.rs
@@ -8,16 +8,30 @@ fn main() -> io::Result<()> {
     let reader = BufReader::new(file);
 
     let mut distances: HashMap<(String, String), i32> = HashMap::new();
+    let mut cities: Vec<String> = Vec::new();
     for line in reader.lines() {
         let line = line?;
         let parts: Vec<&str> = line.split("to").flat_map(|s| s.split("=")).map(str::trim).collect();
-        distances.insert((parts[0].to_string(), parts[1].to_string()), parts[2].parse().unwrap());
+        let city1 = parts[0].to_string();
+        let city2 = parts[1].to_string();
+
+        distances.insert((city1.clone(), city2.clone()), parts[2].parse().unwrap());
+        distances.insert((city2.clone(), city1.clone()), parts[2].parse().unwrap());
+
+        if !cities.contains(&city1) {
+            cities.push(city1);
+        }
+
+        if !cities.contains(&city2) {
+            cities.push(city2);
+        }
+
+
+
     }
 
     let min_distance = 0;
-    for i in 0..distances.len() {
 
-    }
 
     println!("First Answer: {}", min_distance);
 
